daf1e2bc1dff9c269178b0f9286862f0bbaa5054
dianye <1705087628@qq.com>
2023-02-07 16:50:03 +0000

    update src/main/java/cn/lpc/Main.java.
    Signed-off-by: dianye <1705087628@qq.com>

--- src/main/java/cn/lpc/Main.java
+++ src/main/java/cn/lpc/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println("master2");
         System.out.println("Hello world!");
         System.out.println("hot-fix change push");
+         System.out.println("gitee change push");
     }
 }
