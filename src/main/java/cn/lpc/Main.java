13fa145b28ceab9b300053198981c14ff2b8fadd
lipengcheng123 <dianyecr@qq.com>
2023-02-08 00:32:33 +0800

    hot-fix push test first commit

--- src/main/java/cn/lpc/Main.java
+++ src/main/java/cn/lpc/Main.java
@@ -12,8 +12,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("master1");
         System.out.println("master2");
-
-
         System.out.println("Hello world!");
+        System.out.println("hot-fix change push");
     }
 }
