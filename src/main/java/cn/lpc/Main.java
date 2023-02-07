7d0cf771b5b0149448ec6a7616f81e3569424b8a
lipengcheng123 <dianyecr@qq.com>
2023-02-07 23:33:05 +0800

    first commit

---
+++ src/main/java/cn/lpc/Main.java
@@ -0,0 +1,18 @@
+package cn.lpc;
+
+/**
+ * @author dianYe
+ * @version 1.0.0
+ * @date ${YEAR}年${MONTH}月${DAY}日 ${HOUR}:${MINUTE}:${SECOND}
+ * @packageName cn.lpc
+ * @className ${NAME}
+ * @describe TODO
+ */
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/cn/lpc/com/GitTest.java
@@ -0,0 +1,15 @@
+package cn.lpc.com;
+
+/**
+ * @author dianYe
+ * @version 1.0.0
+ * @date 2023年02月07日 23:30:06
+ * @packageName cn.lpc.com
+ * @className GitTest
+ * @describe TODO
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
