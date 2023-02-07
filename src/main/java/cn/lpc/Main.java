605226e66833415f9d6014de78d5dd3dc2a7aed6
lipengcheng123 <dianyecr@qq.com>
2023-02-07 23:48:58 +0800

    master change first commit

--- src/main/java/cn/lpc/Main.java
+++ src/main/java/cn/lpc/Main.java
@@ -10,7 +10,8 @@ package cn.lpc;
  */
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("master1");
+        System.out.println("master2");
 
 
         System.out.println("Hello world!");
